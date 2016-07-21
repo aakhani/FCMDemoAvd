3aded728fa8c03e4ab2af0f519c1c7ab2f02699c
Avdhesh AKhani <avdhesh.akhani123@gmail.com>
2016-07-21 13:32:24 -0400

    FireBase Setup classes

---
+++ src/main/java/com/fcmdemoavd/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.fcmdemoavd;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Avdhesh AKhani on 7/21/2016.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        sendRegistrationToServer(refreshedToken);
+        Log.d(TAG,"Refreshed token: " + refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
